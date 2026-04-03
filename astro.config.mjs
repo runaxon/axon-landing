e21264430226f6128d77e1aec7f6d0550bba9e01
Anthony Meyer <anthonypaulmeyer@gmail.com>
2026-04-03 10:33:10 -0400

    Added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
   site: 'https://runaxon.com',
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
